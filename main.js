fd14d24362f342b6d75a441fa411904cf49af86d
danvb112 <danielbonasser@gmail.com>
2020-02-07 13:30:40 -0300

    JavaScripit ES6

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
-
-class teste {
-    metodo () {
-
-    }
-
-    outro () {
+class List {
+    constructor() {
+        this.data = []
 
     }
+    
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
 
+    }
 }
+
+
+class TodoList extends List {
+    constructor() {
+        super();
+        this.usuario = 'Daniel';
+    }
+
+    mostraUsuario () {
+        console.log(this.usuario);
+    }
+
+
+   
+    }
+
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novoTodo').onclick = function() {
+    MinhaLista.add("Novo Todo");
+}
+
+MinhaLista.mostraUsuario();
