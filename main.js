323652c080d8ecc22b6311f76173317f89c43097
Daniel Bonasser <danielbonasser@gmail.com>
2020-02-09 19:56:33 -0300

    JavaScript ES6

--- main.js
+++ main.js
@@ -14,3 +14,12 @@ console.log(nome);
 console.log(idade);
 console.log(rua);
 console.log(estado);
+
+// SPREAD
+
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+
+const arr3 = [...arr1, ...arr2];
+
+console.log(arr3);
